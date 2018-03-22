1dcc896ea6715cccdc94ce70f23a9a523bd2192b
uca <uca@uca.sv>
2018-03-21 23:28:28 -0600

    Initial commit

---
+++ app/src/main/java/com/agmeag/a00002816tareaclase1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.agmeag.a00002816tareaclase1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
